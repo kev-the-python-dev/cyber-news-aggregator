ed866f59f02536f005fc94968fc520aca4d6abbf
Kev the Python Dev <101847446+kev-the-python-dev@users.noreply.github.com>
2022-05-23 23:39:44 -0700

    Update settings.py

--- tech_news_aggregator/tech_news_aggregator/settings.py
+++ tech_news_aggregator/tech_news_aggregator/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vlrlm9*7%g@@=%m@@64++=i0!^@#-r_m!xnereu@^jugd6^v5x'
+SECRET_KEY = '<secret key>'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
