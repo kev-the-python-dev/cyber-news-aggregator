05b24495ce135186bd10b68e2aa4e629bb3dff71
Kev the Python Dev <101847446+kev-the-python-dev@users.noreply.github.com>
2022-05-24 20:24:32 -0700

    added additional comments
    Comments added to help in readability for those unfamiliar with Django

--- tech_news_aggregator/news/views.py
+++ tech_news_aggregator/news/views.py
@@ -1,18 +1,20 @@
 import requests
 from django.shortcuts import render, redirect
-from bs4 import BeautifulSoup as beautsoup
+from bs4 import BeautifulSoup as beautsoup # Scrapy is better for this, as we can crawl multiple sites
 from news.models import Headline
 
-def article_scrape(request):
-    session = requests.Session()
-    session.headers = {"User-Agent" : "Googlebot/2.1; +https://www.google.com/bot.html)"}
-    url = "https://www.thehackernews.com"
+def article_scrape(request): # This is specifically for TheHackerNews.com. 
+    # For additional websites, create additional functions that mirror this function's 'function' although BS4 selector changes will be needed.
+    session = requests.Session() # Session object performs far better due to cookie persistance and re-useable TCP connections 
+    session.headers = {"User-Agent" : "Googlebot/2.1; +https://www.google.com/bot.html)"} # Using Googlebot will allow us to freely scrape without being blocked.
+    url = "https://www.thehackernews.com" # If you change this, you'll need to re-do all the selectors BeautifulSoup parses.
 
-    content = session.get(url, verify=False).content
+    # Establishing the response, creating the HTML parser (since identifying html selectors in BS4), and assigning all the "news boxes" from THN to var news.
+    content = session.get(url, verify=False).content 
     soup = beautsoup(content, "html.parser")
     news = soup.find_all('div', {"class":"body-post"})
 
-    
+    # Main Execution for collecting each selector information, converting it into str format, and saving it into Django's SQLite3 DB.
     for news_item in news:
         main = news_item.find_all('a')[0]
         link = main['href']
@@ -28,11 +30,12 @@ def article_scrape(request):
     
     return redirect('../')
 
-
+# Required for displaying each "news item" as a list in our home.html template file found in ../../templates/news/home.html
 def list_news(request):
     headlines = Headline.objects.all()[::1]
     context = {
         "object_list" : headlines
-
     }
-    return render(request, "news/home.html", context)
+    
+    return render(request, "news/home.html", context) # Returns HTTP response whose contents are sent to news/home.html.
+# context argument allows us to call the the list of news articles via home.html
