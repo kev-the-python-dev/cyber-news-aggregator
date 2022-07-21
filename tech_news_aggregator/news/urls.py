7d2ec3a904e99197f2fc2a64ed78e7cd692a8f3a
kev-the-python-dev <kingcobrapy@protonmail.com>
2022-07-20 21:37:26 -0400

    added reddit scraper and fixed other bugs

--- tech_news_aggregator/news/urls.py
+++ tech_news_aggregator/news/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from news.views import article_scrape, list_news
+from news.views import reddit_scrape, list_news, article_scrape
 
 urlpatterns = [
     path('scrape/', article_scrape, name="scrape"),
+    path('scrape_reddit/', reddit_scrape, name="scrape_reddit"),
     path('',list_news, name="home"),
 ]
--- tech_news_aggregator/news/views.py
+++ tech_news_aggregator/news/views.py
@@ -1,8 +1,48 @@
+from news.models import Headline
+from datetime import datetime
 from django.db import IntegrityError
-import requests
+import praw, requests
+
 from django.shortcuts import render, redirect
 from bs4 import BeautifulSoup as beautsoup # Scrapy is better for this, as we can crawl multiple sites
-from news.models import Headline
+
+
+def reddit_scrape(request):
+    # Unlike the below function, we will be using Reddit API (PRAW) to pull posts
+    user_agent = 'User-Agent: #'
+    reddit = praw.Reddit(client_id='#', client_secret='#', user_agent=user_agent)
+
+    # Pre-reqs
+    top_posts = reddit.subreddit('Python').top(limit=6)
+    
+    # Scraping execution
+    try:
+        for top_post in top_posts:
+            title = top_post.title
+            
+            # Make date human readable
+            publish_date = datetime.utcfromtimestamp(top_post.created)
+            pub_year = publish_date.year
+            pub_month = publish_date.month
+            pub_day = publish_date.day
+
+            link = top_post.permalink
+            img_src = 'https://i.guim.co.uk/img/media/02c5fc2b42591243e6292fc83f8a97ed78807b57/198_0_2000_1200/master/2000.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=d31d7a8f045e54151b84076280aebca8'
+            
+            new_headline = Headline()
+            new_headline.title = title
+            new_headline.url = 'https://www.reddit.com/' + link
+            new_headline.image = img_src
+            new_headline.date = f'{pub_day}/{pub_month}/{pub_year}'
+
+            try:
+                new_headline.save()
+            except Exception as e:
+                print(e)
+    except IntegrityError:
+        print('Duplicate reddit posts found. Ommitting and moving forward.')
+    finally:
+        return redirect('../')
 
 def article_scrape(request): # This is specifically for TheHackerNews.com. 
     # For additional websites, create additional functions that mirror this function's 'function' although BS4 selector changes will be needed.
@@ -31,6 +71,7 @@ def article_scrape(request): # This is specifically for TheHackerNews.com.
             new_headline.url = link
             new_headline.image = img_src
             new_headline.date = item_date
+            print(new_headline)
 
             try:
                 new_headline.save()
@@ -44,10 +85,13 @@ def article_scrape(request): # This is specifically for TheHackerNews.com.
 
 # Required for displaying each "news item" as a list in our home.html template file found in ../../templates/news/home.html
 def list_news(request):
-    headlines = Headline.objects.all()[:6:1]
+    primary_headlines = Headline.objects.all()[:6:1]
+    reddit_headlines = Headline.objects.all()[7:13:1]
     context = {
-        "object_list" : headlines
+        'object_list' : primary_headlines,
+        'reddit_list' : reddit_headlines
+        
     }
     
-    return render(request, "news/home.html", context) # Returns HTTP response whose contents are sent to news/home.html.
+    return render(request, 'news/home.html', context) # Returns HTTP response whose contents are sent to news/home.html.
 # context argument allows us to call the the list of news articles via home.html
